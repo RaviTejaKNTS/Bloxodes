374ef0f11b59667af2ae220290ce0cc6d46a64c1
RaviTejaKNTS <ravitejaknts@gmail.com>
2026-06-21 21:12:15 +0530

    Fix retired lists redirect

--- apps/web/src/app/(site)/lists/[[...slug]]/page.tsx
+++ apps/web/src/app/(site)/lists/[[...slug]]/page.tsx
@@ -1,7 +1,5 @@
 import { permanentRedirect } from "next/navigation";
 
-export const dynamic = "force-static";
-
 export default function RetiredListsRedirectPage() {
   permanentRedirect("/stats");
 }
